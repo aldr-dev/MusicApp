143096f8a9a3bcdc961ac6cf0e4745dd7df7441d
Gordus Alexander <aldrmiller@gmail.com>
2024-09-02 23:21:10 +0600

    реализовал модель Album.ts. Настроил схему.

---
+++ models/Album.ts
@@ -0,0 +1,29 @@
+import mongoose, {Types} from 'mongoose';
+import Artist from './Artist';
+
+const AlbumSchema = new mongoose.Schema({
+  artist: {
+    type: Types.ObjectId,
+    ref: 'Artist',
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const artist = await Artist.findById(value);
+        return Boolean(artist);
+      },
+      message: 'Artist does not exist',
+    }
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  dataRelease: {
+    type: String,
+    required: true,
+  },
+  image: String,
+});
+
+const Album = mongoose.model('Album', AlbumSchema);
+export default Album;
