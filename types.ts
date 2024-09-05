bf655d533403415ddb6309e9813f77e36115aa73
Gordus Alexander <aldrmiller@gmail.com>
2024-09-06 00:48:22 +0600

    homework-83 реализовал сущность User.ts. Типизировал данные.

--- types.ts
+++ types.ts
@@ -1,3 +1,5 @@
+import {Model} from 'mongoose';
+
 export interface ArtistTypes {
   name: string;
   image: string | null;
@@ -16,3 +18,16 @@ export interface TracksTypes {
   title: string,
   duration: string,
 }
+
+export interface UserFields {
+  username: string;
+  password: string;
+  token: string;
+}
+
+export interface UserMethods {
+  checkPassword(password: string): Promise<boolean>;
+  generateToken(): void;
+}
+
+export type UserModel = Model<UserFields, {}, UserMethods>;
