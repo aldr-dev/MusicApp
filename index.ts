aaabdb34750a8b202d3301a7dc1c38423090b937
Gordus Alexander <aldrmiller@gmail.com>
2024-09-06 02:31:36 +0600

    homework-83 реализовал роут users.ts. Настроил запросы на создание пользователя и авторизацию.

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import artistsRouter from './routers/artists';
 import albumsRouter from './routers/albums';
 import tracksRouter from './routers/tracks';
+import usersRouter from './routers/users';
 
 const app = express();
 const port = 8000;
@@ -15,6 +16,7 @@ app.use(express.static('public'));
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
+app.use('/users', usersRouter);
 
 const run = async () => {
   await mongoose.connect(config.database);
